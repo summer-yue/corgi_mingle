ec031076d8263b5aca49b3beffd32d9bef83a295
Summer Yue <yutingyue514@gmail.com>
2017-05-25 16:50:24 -0700

    Complete settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful save.
+      flash[:success] = "You have successfully updated your profile"
+      redirect_to user_url(@user)
+  
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name, 
+                                          email:    email,
+                                          password: 'password',
+                                          password_confirmation: 'password'  } }
+    assert_not flash.empty?
+    assert_redirected_to user_url(@user)
+    follow_redirect!
+
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+
+end
