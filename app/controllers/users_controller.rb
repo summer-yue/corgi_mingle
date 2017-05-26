f070974fe09d1801a7bf9d9ff1d01ca004d26287
Summer Yue <yutingyue514@gmail.com>
2017-05-25 22:05:14 -0700

    Friendly forwarding for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -30,7 +33,7 @@ class UsersController < ApplicationController
       # Handle a successful save.
       flash[:success] = "You have successfully updated your profile"
       redirect_to user_url(@user)
-  
+
     else
       render 'edit'
     end
@@ -41,5 +44,28 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "You didn't log in, bad corgi."
+        redirect_to login_url
+      end
+    end
+
+    #Confirms the correct user request
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        flash[:danger] = "You can't access this page, bad corgi."
+        redirect_to(root_url)
+      end
+    end
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user == current_user
     end
 end
